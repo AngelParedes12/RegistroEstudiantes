0301782052c99e46b8364bcae4c725579b0c1636
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-01-21 15:20:18 -0400

    Crear la interfaz EditEstudianteUIEvent para manejar eventos de edición de estudiantes

---
+++ app/src/main/java/edu/ucne/registroestudiantes/presentation/tareas/edit/EditEstudianteUIEvent.kt
@@ -0,0 +1,8 @@
+package edu.ucne.registroestudiantes.presentation.tareas.edit
+
+sealed interface EditEstudianteUIEvent {
+    data class OnNombreChange(val value: String) : EditEstudianteUIEvent
+    data class OnEmailChange(val value: String) : EditEstudianteUIEvent
+    data class OnEdadChange(val value: String) : EditEstudianteUIEvent
+    data object OnGuardar : EditEstudianteUIEvent
+}
