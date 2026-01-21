2308c54f18f7d1058b80a98981d4959e773468f1
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-01-21 15:21:17 -0400

    Configurar el host de navegación AppNavHost en MainActivity y habilitar la inyección de dependencias con Hilt

--- app/src/main/java/edu/ucne/registroestudiantes/MainActivity.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/MainActivity.kt
@@ -3,46 +3,24 @@ package edu.ucne.registroestudiantes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.registroestudiantes.presentation.navigation.appNavHost.AppNavHost
 import edu.ucne.registroestudiantes.ui.theme.RegistroEstudiantesTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             RegistroEstudiantesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavHost(
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroEstudiantesTheme {
-        Greeting("Android")
     }
 }
