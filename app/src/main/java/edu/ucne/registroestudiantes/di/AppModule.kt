18b727e6d6c88b13c0be87db9dfd9f3a454f40a3
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-01-24 13:38:36 -0400

    Configurando la inyección de dependencias para el repositorio y DAO de asignaturas en AppModule

--- app/src/main/java/edu/ucne/registroestudiantes/di/AppModule.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/di/AppModule.kt
@@ -7,9 +7,12 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.registroestudiantes.data.local.dao.AsignaturaDao
 import edu.ucne.registroestudiantes.data.local.dao.EstudianteDao
 import edu.ucne.registroestudiantes.data.local.database.AppDatabase
+import edu.ucne.registroestudiantes.data.repository.AsignaturaRepositoryImpl
 import edu.ucne.registroestudiantes.data.repository.EstudianteRepositoryImpl
+import edu.ucne.registroestudiantes.domain.repository.AsignaturaRepository
 import edu.ucne.registroestudiantes.domain.repository.EstudianteRepository
 import javax.inject.Singleton
 
@@ -21,13 +24,12 @@ object AppModule {
     @Singleton
     fun provideDatabase(
         @ApplicationContext context: Context
-    ): AppDatabase {
-        return Room.databaseBuilder(
+    ): AppDatabase =
+        Room.databaseBuilder(
             context,
             AppDatabase::class.java,
             "estudiantes_db"
         ).build()
-    }
 
     @Provides
     fun provideEstudianteDao(
@@ -35,10 +37,19 @@ object AppModule {
     ): EstudianteDao = db.estudianteDao()
 
     @Provides
+    fun provideAsignaturaDao(
+        db: AppDatabase
+    ): AsignaturaDao = db.asignaturaDao()
+
+    @Provides
     @Singleton
     fun provideEstudianteRepository(
         dao: EstudianteDao
-    ): EstudianteRepository {
-        return EstudianteRepositoryImpl(dao)
-    }
+    ): EstudianteRepository = EstudianteRepositoryImpl(dao)
+
+    @Provides
+    @Singleton
+    fun provideAsignaturaRepository(
+        dao: AsignaturaDao
+    ): AsignaturaRepository = AsignaturaRepositoryImpl(dao)
 }
