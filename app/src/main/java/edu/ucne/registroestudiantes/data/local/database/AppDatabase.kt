f08b05cc933991ac16d58928f6b30730365fe08f
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-01-24 13:38:20 -0400

    Se actualizo la base de datos `AppDatabase` para incluir la entidad `AsignaturaEntity` y su respectivo DAO `AsignaturaDao`

--- app/src/main/java/edu/ucne/registroestudiantes/data/local/database/AppDatabase.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/data/local/database/AppDatabase.kt
@@ -2,15 +2,17 @@ package edu.ucne.registroestudiantes.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registroestudiantes.data.local.dao.AsignaturaDao
 import edu.ucne.registroestudiantes.data.local.dao.EstudianteDao
 import edu.ucne.registroestudiantes.data.local.entities.EstudianteEntity
-
+import edu.ucne.registroestudiantes.data.local.entities.AsignaturaEntity
 
 @Database(
-    entities = [EstudianteEntity::class],
+    entities = [EstudianteEntity::class, AsignaturaEntity::class],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun estudianteDao(): EstudianteDao
+    abstract fun asignaturaDao(): AsignaturaDao
 }
