1411af52ccb72e9c0d30f8d08f5f417a5ff0d68b
AngelParedes12 <angel_paredes@ucne.edu.do>
2026-01-31 02:17:58 -0400

    Se le cambio el nombre de rutas `Routes` a `Screen`, implementar la serialización para la navegación y agregar las rutas para `TipoPenalidad`

--- app/src/main/java/edu/ucne/registroestudiantes/presentation/navigation/Routes.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.registroestudiantes.presentation.navigation
-
-sealed class Routes(val route: String) {
-    object ListEstudiantes : Routes("list_estudiantes")
-    object EditEstudiante : Routes("edit_estudiante")
-    object ListAsignaturas : Routes("list_asignaturas")
-    object EditAsignatura : Routes("edit_asignatura")
-}
